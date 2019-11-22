37c0e0789350dd8a750755509298a14444153298
Lisa Ostman <peolina@live.com.au>
2019-11-22 22:14:29 +1100

    added so user can choose password length

--- script.js
+++ script.js
@@ -10,10 +10,29 @@ var button = document.querySelector("#generate");
 button.addEventListener("click", buttonClick);
 
 function buttonClick() {
-    var yesLowerCase = confirm("Would you like to include lower case letters?");
+    var textArea = document.querySelector("#cherryRipe");
+    randomString = [];
+    var yesLowerCase = confirm("Would you like to include lower case letters? \n(Using all four of the next options leads to a stronger password)");
     var yesUpperCase = confirm("Would you like to include upper case letters?");
     var yesNumbers = confirm("Would you like to include numbers?");
-    var yesSpecialCharacters = confirm("Would you like to include special characters?")
+    var yesSpecialCharacters = confirm("Would you like to include special characters?");
+
+
+
+
+    var stringLength = 0;
+
+    while (stringLength === 0) {
+        var stringPrompt = prompt("How long do you want your password? (Must be between 8 - 128)");
+        stringLength = parseInt(stringPrompt);
+
+        if (stringLength < 8 || stringLength > 128) {
+            stringLength = 0;
+        } else if (stringLength >= 8 && stringLength <= 128) {
+        } else {
+            stringLength = 0;
+        }
+    }
 
     if (yesLowerCase) {
         charSet = charSet.concat(["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]);
@@ -35,6 +54,12 @@ function buttonClick() {
         //console.log(charSet);
     }
 
+    if (!yesSpecialCharacters && !yesNumbers && !yesUpperCase && !yesLowerCase) {
+        alert("You need to have at least one combination of letters, numbers or characters to generate a password!");
+    }
+
+
+
     for (var i = 0; i < stringLength; i++) {
         var randomIndex = Math.floor(Math.random() * (charSet.length));
         var adding = charSet[randomIndex];
@@ -42,7 +67,6 @@ function buttonClick() {
     }
 
     var wholePassword = randomString.join("");
-    var textArea = document.querySelector("#cherryRipe");
     textArea.value = wholePassword;
 }
 
